c87c13e59cb5d82d4793568e1d782bde349f23d3
parnurzeal <parnurzeal@gmail.com>
2016-06-27 23:15:27 +0700

    Update manipulator script

--- manipulator.sh
+++ manipulator.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
 METHOD=$1
-# 'https://you-pin.firebaseio.com/pin_geofires.json'
+# Original url on firebase 'https://you-pin.firebaseio.com/pin_geofires.json'
 URL=$2
 FILE=$3
 
+# ./manipulator.sh GET api.youpin.city/pins/-KLHYT8SThD-uCAEL_nJ
 if [ "$METHOD" == "GET" ]; then
   curl -i -X GET \
-       -H "Host:you-pin.firebaseio.com" \
         "${URL}"
+# ./manipulator.sh POST api.youpin.city/pins data1.json
 elif [ "$METHOD" == "POST" ]; then
   echo "Send POST request..."
   curl -i -X POST \
     -H "Content-Type: application/json" \
     -d "@${FILE}" \
     "${URL}"
+# ./manipulator.sh PUT api.youpin.city/pins data1.json
 elif [ "$METHOD" == "PUT" ]; then
   echo "Send PUT request..."
   curl -i -X PUT \
     -H "Content-Type: application/json" \
     -d "@${FILE}" \
     "${URL}"
+# ./manipulator.sh SEARCH api.youpin.city/pins
 elif [ "$METHOD" == "SEARCH" ]; then
   echo "Send SEARCH request..."
   curl -i -X GET \
